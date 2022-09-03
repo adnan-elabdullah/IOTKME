9584a18fd44cdee586aa5079a7412b0fc5ec1e9a
adnan elabdullah <101407361+adnan-elabdullah@users.noreply.github.com>
2022-09-03 15:31:15 +0300

    a

--- examples/Updatedevice/Updatedevice.ino
+++ examples/Updatedevice/Updatedevice.ino
@@ -1,5 +1,4 @@
 #include <IOTKME.h>
-#define updatepin 4 
 int relaypin=13;
 const char* ss     = "wifissid";
 const char* sp = "password";
@@ -7,9 +6,9 @@ IOTKME IOTKME(relaypin);
 void setup() {
 IOTKME.connect(ss,sp);
 //rest device and press on update but for update your device
-if(digitalRead(updatepin)==false){
+
 IOTKME.recupdate("BIN FILE URL HERE");
-}
+
 }
 
 void loop() {
